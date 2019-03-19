0b3ecd0d3a56b232faf7f0efac1703fc647f690a
Roman <sisikro@gmail.com>
2019-03-19 09:06:50 +0100

    adjusted code to changes in implicit broadcast limitaitons in pie

--- app/src/main/java/eu/sisik/devowner/UpdateReceiver.kt
+++ app/src/main/java/eu/sisik/devowner/UpdateReceiver.kt
@@ -1,5 +1,6 @@
 package eu.sisik.devowner
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,16 +14,9 @@ class UpdateReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
-        Log.d("devowner", "UpdateReceiver:" + intent?.dataString)
-
-        // First check that your package got updated and not some
-        // other package
-        if (intent?.dataString == "package:" + context.packageName) {
-
-            // Restart your app here
-            val i = Intent(context, MainActivity::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(i)
-        }
+        // Restart app here
+        val i = Intent(context, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(i)
     }
 }
